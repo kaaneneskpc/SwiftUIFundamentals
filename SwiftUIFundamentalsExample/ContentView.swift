5ac52622a72fc621ce0ba60ca39747a6437ad77b
Kaan Enes KAPICI <kaaneneskpc1@gmail.com>
2023-01-26 19:01:43 +0300

    Initial Commit

---
+++ SwiftUIFundamentalsExample/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUIFundamentalsExample
+//
+//  Created by T66079 on 26.01.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIFundamentalsExample/SwiftUIFundamentalsExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFundamentalsExampleApp.swift
+//  SwiftUIFundamentalsExample
+//
+//  Created by T66079 on 26.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFundamentalsExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
